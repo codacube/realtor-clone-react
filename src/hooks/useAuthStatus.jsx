fa4e899efe7f509467677cd8ac2f96567ac0e0a4
codacube <js@codacube.co.nz>
2025-03-24 19:28:50 +1300

    Added the edit functionality to the profile page

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,6 +1,7 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { useEffect, useState } from "react"
 
+// We are returning two values, so we don't need the 'default' keyword
 export function useAuthStatus() {
     const [loggedIn, setLoggedIn] = useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
