9305a97761a6bb4c475aa8b30d4ac7a386b7055f
codacube <js@codacube.co.nz>
2025-04-02 16:56:49 +1300

    Created EditListing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PrivateRoute from "./components/PrivateRoute"
 import ForgotPassword from "./pages/ForgotPassword"
 import Offers from "./pages/Offers"
 import CreateListing from "./pages/CreateListing"
+import EditListing from "./pages/EditListing"
 import Header from "./components/Header"
 import { ToastContainer, Bounce } from "react-toastify"
 
@@ -27,6 +28,9 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/create-listing" element={<PrivateRoute /> } >
             <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute /> } >
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
