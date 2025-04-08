ae3afa7568b5579b1d242eea0cefe84a7072daa8
codacube <js@codacube.co.nz>
2025-04-08 12:44:47 +1200

    Fixed leaflet popup

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -187,13 +187,13 @@ export default function Listing() {
                     style={{ height: "100%", width: "100%" }}
                 >
                     <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+                        <Popup>
+                            {listing.address}
+                        </Popup>
                     </Marker>
                 </MapContainer>
                 </div>
