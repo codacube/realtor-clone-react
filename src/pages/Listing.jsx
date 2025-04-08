98a11e9fcced39b14a78d32c6357f60af9a885d8
codacube <js@codacube.co.nz>
2025-04-08 12:40:28 +1200

    Added a map to the listing page using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -15,6 +15,7 @@ import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Contact from "../components/Contact"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 export default function Listing() {
     const auth = getAuth()
@@ -178,8 +179,23 @@ export default function Listing() {
                 </div>
                 
                 
-                <div className="bg-blue-300 w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden">
-                    
+                <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+                <MapContainer
+                    center={[listing.geolocation.lat, listing.geolocation.lng]}
+                    zoom={13}
+                    scrollWheelZoom={false}
+                    style={{ height: "100%", width: "100%" }}
+                >
+                    <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                    </Marker>
+                </MapContainer>
                 </div>
             </div>
         </main>
