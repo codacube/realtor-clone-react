572d59437ddc356da0d24a0956fedd3784cb2852
codacube <js@codacube.co.nz>
2025-04-10 14:28:33 +1200

    Created the places for rent sale pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import EditListing from "./pages/EditListing"
 import Header from "./components/Header"
 import { ToastContainer, Bounce } from "react-toastify"
 import Listing from "./pages/Listing"
+import Category from "./pages/Category"
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/category/:categoryName/:listingID" element={<Listing />} />
           <Route path="/create-listing" element={<PrivateRoute /> } >
             <Route path="/create-listing" element={<CreateListing />} />
